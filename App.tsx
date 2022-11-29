d145bd61a4a5d3bd70deda2b6d062a05934e9228
Yasin Torun <yasintorun4680@gmail.com>
2022-11-30 00:15:53 +0300

    Added gottom bottom modal

--- App.tsx
+++ App.tsx
@@ -6,15 +6,18 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as StoreProvider } from 'react-redux';
 import Launch from './Launch';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 const App = () => {
   return (
     <StoreProvider store={store}>
-      <GestureHandlerRootView style={{flex: 1}}>
-        <SafeAreaProvider style={{ flex: 1, backgroundColor: COLORS.background }}>
-          <StatusBar backgroundColor={COLORS.background} />
-          <Launch />
-        </SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <SafeAreaProvider style={{ flex: 1, backgroundColor: COLORS.background }}>
+            <StatusBar backgroundColor={COLORS.background} />
+            <Launch />
+          </SafeAreaProvider>
+        </BottomSheetModalProvider>
       </GestureHandlerRootView>
     </StoreProvider>
   );
